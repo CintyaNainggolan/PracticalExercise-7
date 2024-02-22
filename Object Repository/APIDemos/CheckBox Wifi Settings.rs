37063f013f7aa69af15a8c48f9338c869743757c
Cintya Nainggolan <cintyaysn277@gmail.com>
2024-02-23 00:27:07 +0700

    Tugas Sesi 7

---
+++ Object Repository/APIDemos/CheckBox Wifi Settings.rs
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<MobileElementEntity>
+   <description></description>
+   <name>CheckBox Wifi Settings</name>
+   <tag></tag>
+   <elementGuidId>5f4ce6d5-ecd7-4f1f-89bc-009405d90ed1</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <locator>//android.widge.CheckBox[@resource-id=&quot;android:id/checkbox&quot;]</locator>
+   <locatorStrategy>XPATH</locatorStrategy>
+</MobileElementEntity>
